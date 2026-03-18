7083a16c3120e9baed3233c2dfca8ad6f4c4976b
rakhi <rakhisingh876.com@gmail.com>
2026-03-18 19:00:03 +0530

    Initial Discord bot setup

---
+++ backend/index.js
@@ -0,0 +1,17 @@
+const { Client, GatewayIntentBits } = require("discord.js");
+
+const client = new Client({
+    intents: [
+        GatewayIntentBits.Guilds,
+        GatewayIntentBits.GuildMessages,
+        GatewayIntentBits.MessageContent
+    ]
+});
+
+client.once("ready",()=>{
+    console.log("Bot is ready");
+});
+
+client.login("YOUR_BOT_TOKEN"); 
+
+
