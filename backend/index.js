1ef9c5091d2b03928c3f749ab145a59c8ecdb464
rakhi <rakhisingh876.com@gmail.com>
2026-03-18 19:56:09 +0530

    Added message logging

--- backend/index.js
+++ backend/index.js
@@ -1,17 +1,20 @@
+require("dotenv").config();
 const { Client, GatewayIntentBits } = require("discord.js");
 
 const client = new Client({
     intents: [
-        GatewayIntentBits.Guilds,
-        GatewayIntentBits.GuildMessages,
-        GatewayIntentBits.MessageContent
+        GatewayIntentBits.Guilds,   //know ur server
+        GatewayIntentBits.GuildMessages,  //know when someone message
+        GatewayIntentBits.MessageContent    // see the actual text
     ]
 });
 
-client.once("ready",()=>{
-    console.log("Bot is ready");
+client.once("ready", () => {
+    console.log("Bot is ready ");
 });
 
-client.login("YOUR_BOT_TOKEN"); 
+client.on("messageCreate", (message) => {
+    console.log(`Message received: ${message.content}`);
+});
 
-
+client.login(process.env.DISCORD_BOT_TOKEN);
